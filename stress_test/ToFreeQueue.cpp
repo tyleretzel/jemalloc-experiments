900005bc85b5c29418cd341c87fd986ceb721b55
Tyler Etzel <tyleretzel1@gmail.com>
2018-06-05 16:31:44 -0700

    Added multithreading - Mixer creates producer consumer relations: picks a random producer, and then a random consumer to free the memory allocated in the producer.

---
+++ stress_test/ToFreeQueue.cpp
@@ -0,0 +1,18 @@
+#include "ToFreeQueue.h"
+
+#include <chrono>
+#include <thread>
+
+void ToFreeQueue::free() {
+	std::lock_guard<std::mutex> guard(this->lock_);
+
+  while (!this->q_.empty() &&
+         this->q_.top().freeAfter() < std::chrono::high_resolution_clock::now()) {
+    this->q_.pop();
+  }
+}
+
+void ToFreeQueue::addToFree(Allocation a) {
+	std::lock_guard<std::mutex> guard(this->lock_);
+  this->q_.push(std::move(a));
+}
---
+++ stress_test/ToFreeQueue.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <queue>
+#include <thread>
+
+#include "Producers.h"
+
+class ToFreeQueue {
+public:
+	// frees all allocations whose lifetime has elapsed
+	void free();
+	// Add an allocation to be freed after a particular time
+  void addToFree(Allocation a);
+
+private:
+	std::mutex lock_;
+	std::priority_queue<Allocation, std::vector<Allocation>, std::greater<Allocation>> q_;
+};
